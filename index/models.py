6db5aa49c5ee48f7b532d1b000fb7c480a009918
Kamich-GP <yurochkagameplus@gmail.com>
2025-10-28 21:38:35 +0500

    urls, views

--- index/models.py
+++ index/models.py
@@ -3,28 +3,32 @@ from django.db import models
 # Create your models here.
 # Таблица категорий
 class Category(models.Model):
-    category_name = models.CharField(max_length=32)
+    category_name = models.CharField(max_length=32, verbose_name='Название категории')
     added_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.category_name
 
     class Meta:
+        verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
 # Таблица продуктов
 class Product(models.Model):
-    product_name = models.CharField(max_length=128)
-    product_des = models.TextField()
-    product_count = models.IntegerField()
-    product_price = models.IntegerField()
-    product_photo = models.ImageField(upload_to='media')
-    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=128, verbose_name='Название')
+    product_des = models.TextField(verbose_name='Описание')
+    product_count = models.IntegerField(verbose_name='Кол-во товара')
+    product_price = models.IntegerField(verbose_name='Цена товара')
+    product_photo = models.ImageField(upload_to='media', verbose_name='Фото')
+    product_category = models.ForeignKey(Category,
+                                         on_delete=models.CASCADE,
+                                         verbose_name='Категория')
 
     def __str__(self):
         return self.product_name
 
     class Meta:
+        verbose_name = 'товар'
         verbose_name_plural = 'Товары'
 
 # Таблица корзины
@@ -37,4 +41,5 @@ class Cart(models.Model):
         return str(self.user_id)
 
     class Meta:
+        verbose_name = 'корзина'
         verbose_name_plural = 'Корзины'
--- index/views.py
+++ index/views.py
@@ -2,11 +2,34 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
+# Главная страница
 def home_page(request):
+    # Достаем данные из БД
     all_categories = Category.objects.all()
     all_products = Product.objects.all()
+    # Передаем данные на Frontend
     context = {
         "categories": all_categories,
         "products": all_products
     }
     return render(request, 'home.html', context)
+
+# Страница с выбранной категорией
+def category_page(request, pk):
+    # Достаем данные из БД
+    category = Category.objects.get(id=pk)
+    products = Product.objects.filter(product_category=category)
+    # Передаем данные на Frontend
+    context = {
+        "category": category,
+        "products": products
+    }
+    return render(request, 'category.html', context)
+
+# Страница с выбранным товаром
+def product_page(request, pk):
+    # Достаем данные из БД
+    product = Product.objects.get(id=pk)
+    # Передаем данные на Frontend
+    context = {"product": product}
+    return render(request, 'product.html', context)
